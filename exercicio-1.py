4b2b4fb76438ba2042d1ac33645b002e20b930e7
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2025-11-22 22:38:54 -0300

    adds compact if in exercise 1

--- exercicio-1.py
+++ exercicio-1.py
@@ -1,8 +1,13 @@
+# Usando if "padrão"
 def even_or_odd(number):
     if number % 2 == 0:
         return "Even"
     
     return "Odd"
+
+#Usando if "compacto"
+# def even_or_odd(number):
+#     return "Even" if number % 2 == 0 else "Odd"
 
 print(even_or_odd(1))
 print(even_or_odd(2))
