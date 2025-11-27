c1e41738454db24713e427ff7d53337f7c869f26
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2025-11-27 04:26:26 -0300

    renames exercise 5 file

---
+++ exercicio-5.py
@@ -0,0 +1,25 @@
+# Abordagem mostrada na aula
+def find_needle(haystack):
+    position = 0
+
+    for element in haystack:
+        if element == "needle":
+            break
+        
+        position += 1
+
+    return f"found the needle at position {position}"
+
+# Abordagem alternativa, usando a f
+# def find_needle(haystack):
+#     position = haystack.index("needle")
+#     return f"found the needle at position {position}"
+
+input_list = ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]
+print(find_needle(input_list))
+
+input_list = ["needle", "junk", "hay", "hay", "moreJunk", "hay", "randomJunk"]
+print(find_needle(input_list))
+
+input_list = ["hay", "junk", "hay", "hay", "moreJunk", "hay", "needle"]
+print(find_needle(input_list))
