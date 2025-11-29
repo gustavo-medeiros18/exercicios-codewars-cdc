b60c12e9a091066e70decff9208eed0c4ea74910
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2025-11-29 00:54:43 -0300

    renames exercise 6 file

--- exercicio-6.py
+++ exercicio-6.py
@@ -1,21 +1,23 @@
-def century(year):
-    first_digits = year // 100
-    last_digits = year % 100
+# Abordagem mostrada na aula
+def invert(list):
+    inverted_list = []
 
-    century = first_digits
+    for item in list:
+        inverted_item = item * -1
+        inverted_list.append(inverted_item)
+    
+    return inverted_list
 
-    if last_digits > 0:
-        century += 1
+# Abordagem alternativa, usando list comprehension
+# def invert(list):
+#     inverted_list = [item * -1 for item in list]
+#     return inverted_list
 
-    return century
+input_list = [1, 2, 3, 4, 5]
+print(invert(input_list))
 
-print(century(1705))
-print(century(1900))
-print(century(1601))
-print(century(2000))
-print(century(2742))
-print(century(89))
-print(century(356))
-print(century(20500))
-print(century(20512))
-print(century(1))
+input_list = [1, -2, 3, -4, 5]
+print(invert(input_list))
+
+input_list = []
+print(invert(input_list))
